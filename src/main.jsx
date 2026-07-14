7a3b6db0c8e05dacbd4e8b4e5c4f034a0b989079
Yefersn15 <yefersonandres225@gmail.com>
2026-07-14 01:38:40 -0500

    gitphages

--- src/main.jsx
+++ src/main.jsx
@@ -6,10 +6,11 @@ import "bootstrap/dist/js/bootstrap.bundle.min.js";
 import App from './App.jsx';
 
 const root = createRoot(document.getElementById('root'));
+const basename = import.meta.env.PROD ? '/Proyecto_Five' : '/';
 
 root.render(
   <React.StrictMode>
-    <BrowserRouter basename='/Proyecto_Five/'>
+    <BrowserRouter basename={basename}>
       <App />
     </BrowserRouter>
   </React.StrictMode>
