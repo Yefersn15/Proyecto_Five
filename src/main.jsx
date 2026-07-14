985ecd4608da95c0944bf30a98ca3ea56861c484
Yefersn15 <yefersonandres225@gmail.com>
2026-07-14 01:23:32 -0500

    gitpages

--- src/main.jsx
+++ src/main.jsx
@@ -9,7 +9,7 @@ const root = createRoot(document.getElementById('root'));
 
 root.render(
   <React.StrictMode>
-    <BrowserRouter basename='/Proyecyo_Five/'>
+    <BrowserRouter basename='/Proyecto_Five/'>
       <App />
     </BrowserRouter>
   </React.StrictMode>
--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: "/Proyecyo_Five/",
+  base: '/Proyecto_Five/',
 })
